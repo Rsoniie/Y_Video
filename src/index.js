b0944783e674ad74a5904af6b47ab920e344ac5b
Rsoniie <roshansoniee@gmail>
2024-09-10 19:55:18 +0530

    checking

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 
 import 'dotenv/config'
+import express from 'express'
 import connectDB from "./db/index.js";
 import { app } from './app.js';
 connectDB().then(() => {
     app.listen(process.env.PORT || 8000, () => {console.log(`server is running at port ${process.env.PORT}`)})
 }).catch((err) => console.log(err));
+
+
+const app = express();
+app.get('/', (req, res) => {
+    res.send("App is running");
+})
